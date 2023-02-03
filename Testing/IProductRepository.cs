3ba83724e7436416c42ca50bddf5145b257471d7
Ross DiBartolo <dibartoloross@gmail.com>
2023-02-03 16:41:22 -0500

    step 3

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -6,6 +6,7 @@ namespace Testing
     {
         public IEnumerable<Product> GetAllProducts();
         public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
         
     }
  
